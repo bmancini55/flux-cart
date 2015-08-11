5c00672e1c8eb934b94c19734881c4b4b7a54657
Brian Mancini <bmancini@gmail.com>
2015-08-11 08:18:50 -0400

    Added link to details to item list item

--- client/js/components/ItemListItem.jsx
+++ client/js/components/ItemListItem.jsx
@@ -1,5 +1,7 @@
 
 let React       = require('react');
+let Router      = require('react-router');
+let Link        = Router.Link;
 let CartActions = require('../actions/CartActions');
 
 let ItemListItem = React.createClass({
@@ -8,7 +10,9 @@ let ItemListItem = React.createClass({
     let item = this.props.item;
     return (
       <div className="shop-item col-md-3 col-sm-4 col-xs-6">
-        <img className="img-responsive item-img" src={item.imagePath} />
+        <Link to="item-details" params={{ itemId: item.id }}>
+          <img className="img-responsive item-img" src={item.imagePath} />
+        </Link>
         <h3 className="shop-item-title">{item.title}</h3>
         <p className="shop-item-subtitle">{item.publisher}</p>
         <button onClick={this.addToCart}>Add to Cart</button>
